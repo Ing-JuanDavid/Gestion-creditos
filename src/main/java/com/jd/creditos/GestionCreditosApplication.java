2f471fef2e597188f6e1a763fe0a4eea552939bb
JuanDavid <juandavidsalgadoromero745@gmail.com>
2025-02-22 17:45:36 -0500

    Se crearon todos los metodos de la app en la implementacion de consola, solo resta optimizar el metodo 'registrarPago'

--- src/main/java/com/jd/creditos/GestionCreditosApplication.java
+++ src/main/java/com/jd/creditos/GestionCreditosApplication.java
@@ -9,21 +9,17 @@ import com.jd.creditos.servicios.IPagoServ;
 import com.jd.creditos.servicios.imp.ClienteServImp;
 import com.jd.creditos.servicios.imp.CreditoServImp;
 import com.jd.creditos.servicios.imp.PagoServImp;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class GestionCreditosApplication implements CommandLineRunner {
@@ -47,17 +43,17 @@ public class GestionCreditosApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		iniciarAppGestionCreditos();
+		logger.info(nl + nl+"<----Gestion de creditos---->" + nl);
+		AppGestionCreditos();
 	}
 
-	public void iniciarAppGestionCreditos(){
-		Scanner ent = new Scanner(System.in);
-		boolean salir = false;
-		logger.info(nl + nl+"<----Gestion de creditos---->" + nl);
+	public void AppGestionCreditos(){
+		var consola = new Scanner(System.in);
+		var salir = false;
 		while(!salir){
 			try{
-				var opcion = mostrarMenu(ent);
-				salir = ejecutarOpciones(ent, opcion);
+				var opcion = mostrarMenu(consola);
+				salir = ejecutarOpciones(consola, opcion);
 			}catch (Exception e){
 				logger.info("Ha ocurrirdo un error: " + e.getMessage() + nl);
 			}finally{
@@ -66,49 +62,37 @@ public class GestionCreditosApplication implements CommandLineRunner {
 		}
 	}
 
-	public int mostrarMenu(Scanner ent){
-
+	public int mostrarMenu(Scanner consola){
 		logger.info("""
 				\t.:MENU:.
 				1. Agregar cliente
 				2. Crear un credito
 				3. Registrar pago
 				4. Ver clientes
-				5. Salir
+				5. Buscar creditos por cliente
+				6. Salir
 				Ecoje una opcion:\s""");
-		return Integer.parseInt(ent.nextLine());
+		return Integer.parseInt(consola.nextLine());
 	}
 
-	public boolean ejecutarOpciones(Scanner ent, int opcion){
+	public boolean ejecutarOpciones(Scanner consola, int opcion){
 		switch(opcion){
-			case 1 -> agregarCliente(ent);
-			case 2 -> {
-				if(mostrarClientes())
-					crearCredito(ent);
-				else
-					logger.info("Debe agregar un cliente primero" + nl);
-			}
-			case 3 ->{
-				if(mostrarClientes())
-					registrarPago(ent);
-				else
-					logger.info("Debe agregar un cliente primero" + nl);
-			}
-			case 4 -> {
-				if(!mostrarClientes())logger.info("No hay clientes para mostrar" + nl);}
-			case 5 ->{
-				logger.info("Saliendo..." +nl);
-				return true;}
+			case 1 -> agregarCliente(consola);
+			case 2 -> crearCredito(consola);
+			case 3 -> registrarPago(consola);
+			case 4 -> {if(!mostrarClientes())logger.info("No hay clientes para mostrar" + nl);}
+			case 5 -> buscarCreditosCliente(consola);
+			case 6 -> {logger.info("Saliendo..." +nl);return true;}
 		}
 		return false;
 	}
 
-	public void agregarCliente(Scanner ent){
+	public void agregarCliente(Scanner consola){
 		logger.info(nl + "Agregando cliente----->" + nl);
 		logger.info("Id: ");
-		var id = Integer.parseInt(ent.nextLine());
+		var id = Integer.parseInt(consola.nextLine());
 		logger.info("Nombre: ");
-		var nombre = ent.nextLine();
+		var nombre = consola.nextLine();
 		Cliente cliente = new Cliente();
 		cliente.setIdCliente(id);
 		cliente.setNombre(nombre);
@@ -116,20 +100,23 @@ public class GestionCreditosApplication implements CommandLineRunner {
 		logger.info("Cliente agregado" + nl);
 	}
 
-	public void crearCredito(Scanner ent){
+	public void crearCredito(Scanner consola){
+		if(!mostrarClientes()) {
+			logger.info("Debe agregar un cliente primero" + nl);
+			return;
+		}
 		logger.info(nl + "Creando credito----->" + nl);
-		//
 		logger.info("Id del cliente: ");
-		var id = Integer.parseInt(ent.nextLine());
+		var id = Integer.parseInt(consola.nextLine());
 		var cliente = clienteServ.buscarCliente(id);
 
 		if(cliente != null){// Si el cliente existe se recolectan los datos para el credito y se crea
 			logger.info("Monto: ");
-			Double monto = Double.parseDouble(ent.nextLine());
+			Double monto = Double.parseDouble(consola.nextLine());
 			logger.info("Taza de interes: ");
-			float ti = Float.parseFloat(ent.nextLine());
+			float ti = Float.parseFloat(consola.nextLine());
 			logger.info("Fecha final (yy-mm-dd): ");
-			var fecha = ent.nextLine();
+			var fecha = consola.nextLine();
 
 			// Como el metodo setFechaF recibe como argumento un objeto de tipo LocalDate y por consola se obtiene un String,
 			// este se debe convertir al tipo requerido
@@ -150,10 +137,14 @@ public class GestionCreditosApplication implements CommandLineRunner {
 
 	}
 
-	public void registrarPago(Scanner ent){
+	public void registrarPago(Scanner consola){
+		if(!mostrarClientes()) {
+			logger.info("Debe agregar un cliente primero" + nl);
+			return;
+		}
 		logger.info(nl + "Realizar pago------>" + nl);
 		logger.info("Id cliente: ");
-		var idCliente = Integer.parseInt(ent.nextLine());
+		var idCliente = Integer.parseInt(consola.nextLine());
 		Cliente cliente = clienteServ.buscarCliente(idCliente);
 		if(cliente == null){
 			logger.info("No existe un cliente con Id: " + idCliente + nl);
@@ -168,7 +159,7 @@ public class GestionCreditosApplication implements CommandLineRunner {
 		logger.info(nl + "Creditos de " + cliente.getNombre() + " ----->" +nl);
 		creditos.forEach(credito -> logger.info(credito.toString() + nl));
 		logger.info("Seleccione un credito (Id): ");
-		var idCredito = Integer.parseInt(ent.nextLine());
+		var idCredito = Integer.parseInt(consola.nextLine());
 		Credito credito = buscarCreditoCliente(idCliente,idCredito);
 		if(credito == null){
 			logger.info("No existe un credito con Id: " + idCredito + nl);
@@ -178,7 +169,7 @@ public class GestionCreditosApplication implements CommandLineRunner {
 		logger.info(nl + "Efectuar pago-----v" + nl);
 		logger.info(credito.toString() + " seleccionado" + nl);
 		logger.info("Ingrese el valor a cancelar: ");
-		BigDecimal valor = BigDecimal.valueOf(Double.parseDouble(ent.nextLine()));
+		BigDecimal valor = BigDecimal.valueOf(Double.parseDouble(consola.nextLine()));
 
 		var pago  = new Pago();
 		pago.setCliente(cliente);
@@ -196,15 +187,38 @@ public class GestionCreditosApplication implements CommandLineRunner {
 		// ya que los datos faltantes los rellena el trigger
 	}
 
+	public void buscarCreditosCliente(Scanner consola){
+		logger.info(nl + "Buscar creditos----->" + nl);
+		logger.info("Id cliente: ");
+		var idCliente = Integer.parseInt(consola.nextLine());
+		var cliente = clienteServ.buscarCliente(idCliente);
+		if(cliente == null){
+			logger.info("No existe un cliente con id: " + idCliente + nl);
+			return;
+		}
+		logger.info(nl + "Creditos de " + cliente.getNombre() + "-----v" + nl);
+		cliente.getCreditos().forEach( c -> logger.info(c.toString() + nl));
+		logger.info("Seleccione un credito: ");
+		var idCredito = Integer.parseInt(consola.nextLine());
+		var credito = creditoServ.buscarCredito(idCredito);
+		if(credito == null){
+			logger.info("No existe un credito con id: " + idCredito + nl);
+			return;
+		}
+		logger.info(nl + "Historial de pagos-----v" + nl);
+		AtomicInteger cont = new AtomicInteger(0);
+		credito.getPagos().forEach(p-> {
+			logger.info(p.toString() + nl);
+			cont.incrementAndGet();
+		});
+		logger.info("Cantidad de pagos: " + cont.get() + nl);
+	}
+
 	public boolean mostrarClientes(){
 		var clientes = clienteServ.listarClientes();
-		if(! clientes.isEmpty()){
-			logger.info(nl + "Clientes----->" + nl);
-			clientes.forEach(cliente -> logger.info(cliente.toString()+nl));
-		}
-		else{
-			return false;
-		}
+		if(clientes.isEmpty()) return false;
+		logger.info(nl + "Clientes----->" + nl);
+		clientes.forEach(cliente -> logger.info(cliente.toString()+nl));
 		return true;
 	}
 
